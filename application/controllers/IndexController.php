68d77be591f2a7f6b96f7b732c3b96245efa84aa
michael-pixight <michael@pixight.net>
2012-12-06 18:17:42 +0100

    On avance dans le tuto

--- application/controllers/IndexController.php
+++ application/controllers/IndexController.php
@@ -10,17 +10,46 @@ class IndexController extends Zend_Controller_Action
 
     public function indexAction()
     {
-        // action body
+        $albums = new Application_Model_DbTable_Albums();
+        $this->view->albums = $albums->fetchAll();
     }
 
     public function modifierAction()
     {
-        // action body
+        $form = new Application_Form_Album();
+        $form->envoyer->setLabel('Sauvegarder');
+        $this->view->form = $form;
+        if ($this->getRequest()->isPost()) {
+            
+        }else{
+            $id = $this->getParam('id', 0);
+            if($id > 0){
+                $albums = new Application_Model_DbTable_Albums();
+                $form->populate($albums->obtenirAlbum( $id ) );
+            }
+        }
     }
 
     public function ajouterAction()
     {
-        // action body
+        $form = new Application_Form_Album();
+        $form->envoyer->setLabel('Ajouter');
+        $this->view->form = $form;
+        //echo '<pre>', print_r( $this->view->form , 1), '</pre>';
+
+        if ($this->getRequest()->isPost()) {
+            $formData = $this->getRequest()->getPost();
+            if ($form->isValid($formData)) {
+                $artiste = $form->getValue('artiste');
+                $titre = $form->getValue('titre');
+                $albums = new Application_Model_DbTable_Albums();
+                $albums->ajouterAlbum($artiste, $titre);
+
+                $this->_helper->redirector('index');
+            } else {
+                $form->populate($formData);
+            }
+        }
     }
 
     public function supprimerAction()
